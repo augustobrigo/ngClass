d3f9d95e38116b0fe5534db5b99bd07f3b41789b
augustobrigo <profeaugustobriga@gmail>
2024-02-19 13:36:42 +0100

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+cargarClasePrincipal(event: any) {
+  this.claseRecibida=event.ordenHijo;
+}
   title = 'ngClass';
+  clase:string="";
+  claseRecibida:string="";
+
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+enviarclase() {
+this.clasePrincipal.emit({ ordenHijo:this.claseNueva})
+}
+@Input() infocabecera:string="";
+@Output() clasePrincipal=new EventEmitter();
+claseNueva:string="";
+
+}
